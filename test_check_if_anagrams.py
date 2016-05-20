67ee2813b2fc569bf80c15bfe583ba3c8daa72d1
Sumeet Jain <sumeet@sumeetjain.com>
2016-05-20 13:44:40 -0500

    Polish

--- test_check_if_anagrams.py
+++ test_check_if_anagrams.py
@@ -1,9 +1,44 @@
 def are_anagrams(str1, str2):
+  # Remove the `return None` code below and add your own code.
   return None
 
-# -------------------------------------------
+# -----------------------------------------------------------------------------
 
-def test_answer():
-  string1 = "search"
+# These are the automated tests for this exercise. Do not modify them at all.
+# When you run `bin/check`, these tests will report on the validity of your
+# algorithm.
+
+def test_simple_anagram():
+  string1 = "ah"
+  string2 = "ha"
+  assert are_anagrams(string1, string2)
+
+def test_complex_anagram():
+  string1 = "senator"
+  string2 = "treason"
+  assert are_anagrams(string1, string2)
+
+def test_anagram_with_numbers():
+  string1 = "c378ty6op"
+  string2 = "py7tc6o38"
+  assert are_anagrams(string1, string2)
+
+def test_empty_strings():
+  string1 = ""
+  string2 = ""
+  assert are_anagrams(string1, string2)
+
+def test_anagram_with_capital_letters():
+  string1 = "sEaRcH"
   string2 = "chaser"
   assert are_anagrams(string1, string2)
+
+def test_barely_not_an_anagram():
+  string1 = "bird"
+  string2 = "drip"
+  assert are_anagrams(string1, string2) == False
+
+def test_not_an_anagram():
+  string1 = "dismiss"
+  string2 = "excavate"
+  assert are_anagrams(string1, string2) == False

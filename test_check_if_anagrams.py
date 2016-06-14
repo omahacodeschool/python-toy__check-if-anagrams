41f6842c35fd0aa802f65431fc2649a69fe789e6
Matt Hughes <mhughes27@gmail.com>
2016-06-14 20:55:22 +0000

    Submitting Python Anagtram

--- test_check_if_anagrams.py
+++ test_check_if_anagrams.py
@@ -6,8 +6,18 @@ def are_anagrams(str1, str2):
   #Sort both strings alphebetically
   #Compare strings - they are anagrams if they are equal
   
+  low1 = str1.lower()
+  low2 = str2.lower()
   
-  return None
+  abc1 = sorted(low1)
+  abc2 = sorted(low2)
+  
+  if abc1 == abc2:
+    return True
+  else:
+    return False
+  
+    
 
 # -----------------------------------------------------------------------------
 
